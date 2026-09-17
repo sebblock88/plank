8d384a6cd5b899f3fbe39c70add7861bd023dabe
naimaahsan <naimauk@yahoo.co.uk>
2026-09-17 14:01:03 +0100

    made letters guessed bold

--- app/src/main/java/com/example/plank/components/GuessedLettersBox.kt
+++ app/src/main/java/com/example/plank/components/GuessedLettersBox.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -21,7 +22,8 @@ fun GuessedLettersBox(guessed: Set<Char>) {
             horizontalArrangement = Arrangement.Center) {
             Text(
                 "Letters guessed:",
-                fontSize = 22.sp
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
             )
         }
 
